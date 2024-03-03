336d7fdd7c21edcc009381f9b571be0a74f37505
trflorian <florian.trautweiler@hispeed.ch>
2024-03-03 14:25:36 +0100

    draw contours for detected polygons

--- main.py
+++ main.py
@@ -14,28 +14,29 @@ cv2.namedWindow(win_name)
 
 tb_t1 = "Threshold 1"
 tb_t2 = "Threshold 2"
-cv2.createTrackbar(tb_t1, win_name, 200, 255, lambda x: None)
-cv2.createTrackbar(tb_t2, win_name, 255, 255, lambda x: None)
+cv2.createTrackbar(tb_t1, win_name, 200, 255, lambda _: None)
+cv2.createTrackbar(tb_t2, win_name, 255, 255, lambda _: None)
+
+tb_approx = "Approx Poly DP"
+cv2.createTrackbar(tb_approx, win_name, 0, 255, lambda _: None)
 
 while 1:
     t1 = cv2.getTrackbarPos(tb_t1, win_name)
     t2 = cv2.getTrackbarPos(tb_t2, win_name)
+    t_approx = cv2.getTrackbarPos(tb_approx, win_name) / 255.0 / 100
 
     thresh = cv2.threshold(gray, t1, t2, cv2.THRESH_BINARY_INV)[1]
-    cnts, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
     thresh = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
+
     for c in cnts:
         peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.015 * peri, True)
+        approx = cv2.approxPolyDP(c, t_approx * peri, True)
         if len(approx) == 4:
-            x, y, w, h = cv2.boundingRect(approx)
-            cv2.rectangle(thresh, (x, y), (x + w, y + h), (36, 255, 12), 2)
+            cv2.drawContours(thresh, [approx], -1, (0, 255, 0), 3)
 
     cv2.imshow(win_name, thresh)
-
-    # canny = cv2.Canny(image, t1, t2)
-    # cv2.imshow(win_name, canny)
 
     k = cv2.waitKey(1) & 0xFF
     if k == 27:
